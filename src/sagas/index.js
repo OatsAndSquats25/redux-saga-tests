ed2ac51aaebaf1dd0a809a4fcbe8bc8d621cec50
Carl <carl@bunid.ca>
2020-06-21 13:53:59 -0400

    clean

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,8 @@
-import {call, put, takeEvery, takeLatest} from 'redux-saga/effects';
-import {REQUEST_API_DATA, RECEIVE_API_DATA, receiveApiData} from '../actions'
+import {call, put, takeLatest} from 'redux-saga/effects';
+import {REQUEST_API_DATA, receiveApiData} from '../actions'
 import {fetchData} from '../api'
 
-function* getApiData(action) {
+function* getApiData() {
     try {
         const data = yield call(fetchData)
         yield put(receiveApiData(data))

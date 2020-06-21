d95a18ba81b40d0fa4057cb7931262c01cfce11c
Carl <donc_langlois@hotmail.com>
2020-06-21 16:16:00 -0400

    hilary view

--- src/views/HilaryView.js
+++ src/views/HilaryView.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import {requestApiData} from "../actions"
+import Tweet from "../components/Tweet";
 
 const HilaryView = () => {
+    const apiData = useSelector(state => state.api);
+    const dispatch = useDispatch();
 
-  return (
-      <div>
-          Hilary
-      </div>
-  )
+    useEffect(() => {
+        const requestParams = {
+            screen_name: 'HillaryClinton'
+        }
+        dispatch(requestApiData(requestParams))
+    }, [dispatch])
+
+    if (!apiData.tweets) return <div>No tweets</div>
+
+    return (
+        <div>
+            <h1 className="title">
+                {apiData.screen_name}
+            </h1>
+            {apiData.tweets.map(tweet => <Tweet key={tweet.id} tweet={tweet}/>)}
+        </div>
+    )
 }
 
-export default HilaryView;
+export default HilaryView
 
